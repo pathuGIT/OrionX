49219a43c51a65d97122be63049083e0d8060ce3
W.S.A. Wickramasinghe <127072941+Sandali3000@users.noreply.github.com>
2025-03-23 15:51:00 -0700

    menuTypeList backend

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import authRoutes from './routes/authRoutes.js';
 import userRouter from './routes/userRouter.js';
 import mailRouter from './routes/mailRouter.js';
-import menuRoutes from './routes/menuRoutes.js';
+import menuListTypeRoutes from './routes/menuListTypeRoutes.js';
 
 const app = express();
 
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRouter);
 app.use('/api/mail', mailRouter);
-app.use('/api/menus', menuRoutes);
+app.use('/menus', menuListTypeRoutes);
 
 // Global error handler
 app.use((err, req, res, next) => {
