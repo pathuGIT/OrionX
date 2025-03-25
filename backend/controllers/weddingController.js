25d328de5704a89a1b9acad87d634b8f9d441917
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2025-03-25 16:26:41 +0530

    Improve error handling in createWedding function to catch server errors

--- backend/controllers/weddingController.js
+++ backend/controllers/weddingController.js
@@ -2,14 +2,18 @@
 import Wedding from '../models/wedding.js'; // Default import
 
 export const createWedding = (req, res) => {
-    const weddingData = req.body;
-
-    Wedding.createWedding(weddingData, (err) => {
-        if (err) {
-            console.error('Error saving wedding details:', err);
-            return res.status(500).json({ error: 'Database error.' });
-        }
-        res.status(201).json({ message: 'Wedding details saved successfully' });
-    });
+  try {
+      const weddingData = req.body;
+  
+      Wedding.createWedding(weddingData, (err) => {
+          if (err) {
+              console.error('Error saving wedding details:', err);
+              return res.status(500).json({ error: 'Database error.' });
+          }
+          res.status(201).json({ message: 'Wedding details saved successfully' });
+      });
+    } catch (error) {
+        res.status(500).json({ msg: 'Server error...', error });
+    }
 };
 
