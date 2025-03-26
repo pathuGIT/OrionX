29158e03881a491f4bec90190f7a75a1cbbda560
PathumLakshan <92371888+pathuGIT@users.noreply.github.com>
2025-03-27 03:33:46 +0530

    fix the refresh token error

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -66,13 +66,13 @@ export const login = async (req, res) => {
 
         if (user.role == 'customer') {
             const token = jwt.sign({ userId: user.customer_id, userEmail: user.email, role: user.role }, process.env.JWT_SECRET, { expiresIn: '2m' });
-            const refreshToken = jwt.sign({ userId: user.customer_id, userEmail: user.email, role: user.role }, process.env.JWT_REFRESH, { expiresIn: '1h' });
+            const refreshToken = jwt.sign({ userId: user.customer_id, userEmail: user.email, role: user.role }, process.env.JWT_REFRESH, { expiresIn: '30s' });
             
             await saveCustomerRefreshTokenModel(refreshToken, user.customer_id);
             res.status(200).json({ message: 'Login successful', userEmail: user.email, id: user.customer_id, role: user.role, token, refreshToken });
         } else {
             const token = jwt.sign({ userId: user.user_id, userEmail: user.email, role: user.role }, process.env.JWT_SECRET, { expiresIn: '2m' });
-            const refreshToken = jwt.sign({ userId: user.user_id, userEmail: user.email, role: user.role }, process.env.JWT_REFRESH, { expiresIn: '1h' });
+            const refreshToken = jwt.sign({ userId: user.user_id, userEmail: user.email, role: user.role }, process.env.JWT_REFRESH, { expiresIn: '30s' });
     
             await saveSystemuserRefreshTokenModel(refreshToken, user.user_id);
             res.status(200).json({ message: 'Login successful', userEmail: user.email, id: user.user_id, role: user.role, token, refreshToken });    
@@ -106,7 +106,7 @@ export const refresh = async (req, res) => {
         // Access user.id and user.role from the decoded token
         const { useId, userEmail, role } = user;
 
-        const token = jwt.sign({ useId: useId, userEmail: userEmail, role: role }, process.env.JWT_SECRET, { expiresIn: '2m' });
+        const token = jwt.sign({ useId: useId, userEmail: userEmail, role: role }, process.env.JWT_SECRET, { expiresIn: '10s' });
 
         res.status(200).json({ message: 'Token refreshed', userEmail: user.email, id: user.id, role: user.roles, token });
     });
