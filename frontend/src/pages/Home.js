02736a1a1a6324a409825c446bd5512bed2044ef
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2025-03-27 08:44:15 +0530

    Implement customer event planning feature and update navigation

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Home = () => {
   return (
     <div>
-            
+            <p>Welcome Customer Home : {sessionStorage.getItem('credential')}</p>
             
         </div>
   )
