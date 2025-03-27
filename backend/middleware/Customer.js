59704bacd8c6ee65971ec17ba9d43d7e82be917f
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2025-03-27 21:59:51 +0530

    Add customer middleware for authorization and apply to event and wedding routes

---
+++ backend/middleware/Customer.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken';
+
+export const customer = (req, res, next) => {
+    if (!req.headers.authorization) {
+        return res.status(401).json({ message: 'No token provided' }); // Ensure no `next()` call
+    }
+
+    //assign the refreshtoken
+    const token = req.headers.authorization.split(' ')[1];
+
+    //check user role is admin?
+    const decoded = jwt.decode(token);
+    const userRole = decoded?.role;
+    if(userRole !== 'customer'){
+        return res.status(403).json({ message: 'You are not authorized bro..' });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) return res.status(401).json({ message: 'Invalid token' });
+        req.user = user;
+        console.log(user)
+        next(); // Call next only when everything is valid
+    });
+};
