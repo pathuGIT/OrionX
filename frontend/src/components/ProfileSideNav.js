dc8d2ccd4b469c239dae6565b54cb8ec16875b18
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2025-03-30 02:42:14 +0530

    Add ShowEvents route and update ProfileSideNav for event display

--- frontend/src/components/ProfileSideNav.js
+++ frontend/src/components/ProfileSideNav.js
@@ -4,7 +4,7 @@ import { Logout } from '../components/Logout'
 const CustomerSideNav = ({ setDisplay }) => {
   return (
     <div className="border inline-block">
-      <li><button onClick={() => setDisplay('customerEvent')}>Plan the Event</button></li>
+      <li><button onClick={() => setDisplay('ShowEvents')}>Plan the Event</button></li>
       <li><button onClick={() => setDisplay('PlanYourEvent')}>Plan</button></li>
       <li><button>link</button></li>
       <li><button ><Logout/></button></li>
