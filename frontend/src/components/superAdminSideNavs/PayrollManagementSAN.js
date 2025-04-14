b134fda67c36c5736cb2b6e9699b7525a325f7bd
Dilshan samarasinghe <113366721+Dilshanjith@users.noreply.github.com>
2025-04-14 22:37:36 +0530

    Add ServicesChargeCalc component and update PayrollManagementSAN for navigation

--- frontend/src/components/superAdminSideNavs/PayrollManagementSAN.js
+++ frontend/src/components/superAdminSideNavs/PayrollManagementSAN.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import { Logout } from '../../components/Logout'
+import ServicesChargeCalc from '../../pages/superAdmin/ServicesChargeCalc'
 
 const PayrollManagementSAN = ({ setRenderContent }) => {
     useEffect(() => {
@@ -10,6 +11,10 @@ const PayrollManagementSAN = ({ setRenderContent }) => {
             // case 'test':
             //     setRenderContent(() => () => <AddEmployee />);
             //     break;
+            case 'ServicesChargeCalculation':
+                setRenderContent(() => () => <ServicesChargeCalc/>);
+                break;
+
             default:
                 setRenderContent(() => () => <p>Page </p>);
         }
@@ -18,9 +23,9 @@ const PayrollManagementSAN = ({ setRenderContent }) => {
         <div className=''>
             <ul class="flex flex-col py-4 ">
                 <li>
-                    <button onClick={() => handleRenderContent('test')} class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-white hover:text-gray-800">
+                    <button onClick={() => handleRenderContent('ServicesChargeCalculation')} class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-white hover:text-gray-800">
                         <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-white"><i class="bx bx-home"></i></span>
-                        <span class="text-sm font-medium">Test Link</span>
+                        <span class="text-sm font-medium">ServicesCharge</span>
                     </button>
                 </li>
                 <li>
---
+++ frontend/src/pages/superAdmin/ServicesChargeCalc.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ServicesChargeCalc = () => {
+    return (
+        <div>
+            <h1>Services charge calculation</h1>
+        </div>
+    );
+};
+
+export default ServicesChargeCalc;
