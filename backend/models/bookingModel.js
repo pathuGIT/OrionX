754306ff133dd8bb43b8c924d8fa0d89be1e41e6
pathuGIT <wannigelakshan@gmail.com>
2025-04-29 19:31:32 +0530

    Backend done for venue management
    update by id
    check by id
    get by id
    add by id
    get all
    delete by id

--- backend/models/bookingModel.js
+++ backend/models/bookingModel.js
@@ -10,3 +10,45 @@ export const addNewVenue = async ({ name, time, location, minCapacity, maxCapaci
         [name, time, location, minCapacity, maxCapacity, price, today, today]
     );
 };
+
+// Get all venues
+export const getAllVenuesModel = async () => {
+    const [rows] = await pool.query(`SELECT * FROM venue`);
+    return rows;
+};
+
+export const deleteVenueByIdModel = async (id) => {
+    const [result] = await pool.query(`DELETE FROM venue WHERE venue_id = ?`, [id]);
+    return result;
+};
+
+export const checkVenuById = async (id) => {
+    const [rows] = await pool.query(`SELECT * FROM venue WHERE venue_id = ?`, [id]);
+    return rows.length > 0;
+};
+
+export const getVenueByIdModel = async (id) => {
+    const [rows] = await pool.query(`SELECT * FROM venue WHERE venue_id = ?`, [id]);
+    if (rows.length === 0) {
+        return null; // Return null if no result found
+    } else {
+        return rows; // Return the first result
+    }
+};
+
+export const updateNewVenueModel = async ({ id, name, time, location, minCapacity, maxCapacity, price }) => {
+    const today = new Date().toISOString().slice(0, 10);
+    console.log(id, name, time, location, minCapacity, maxCapacity, price );
+    await pool.query(
+        `UPDATE venue SET 
+            venue_name = ?, 
+            time_slot = ?, 
+            Location = ?, 
+            min_capacity = ?, 
+            max_capacity = ?, 
+            price = ?, 
+            updated_at = ?
+         WHERE venue_id = ?`,
+        [name, time, location, minCapacity, maxCapacity, price, today, id]
+    );
+};
--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import { superAdmin } from '../middleware/Super_admin.js';
-import { addVenue } from '../controllers/bookingController.js';
+import { addVenue, deleteVenueById, getAllVenue, getVenueById, updateVenueById } from '../controllers/bookingController.js';
 
 
 const router = express.Router();
 
 router.post('/addVenue', superAdmin,  addVenue);
+router.get('/getAllVenues', superAdmin,  getAllVenue);
+router.delete('/deleteVenueById', superAdmin,  deleteVenueById);
+router.put('/updateVenueById', superAdmin, updateVenueById);
+router.get('/getVenueById', superAdmin, getVenueById);
+    
 
 export default router;
