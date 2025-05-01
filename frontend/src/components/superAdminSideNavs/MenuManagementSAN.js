b1174706dac4b2ff0f28e6c5a88488197dc96a3c
W.S.A. Wickramasinghe <127072941+Sandali3000@users.noreply.github.com>
2025-05-01 15:55:29 +0530

    refactor(MenuManagementSAN): simplify sidebar and remove unused components

--- frontend/src/components/superAdminSideNavs/MenuManagementSAN.js
+++ frontend/src/components/superAdminSideNavs/MenuManagementSAN.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { Logout } from '../../components/Logout'
+// import GetMenus from '../../pages/superAdmin/GetMenus';
 import CreateMenuListType from '../../pages/superAdmin/CreateMenuListType';
 import CreateMenuType from '../../pages/superAdmin/CreateMenuType';
 import CreateCategory from '../../pages/superAdmin/CreateCategories';
 import CreateItem from '../../pages/superAdmin/CreateItem';
 import CreateCategoryMenuType from '../../pages/superAdmin/CreateCategoryMenuType';
-import CreateItemCategoryMenuType from '../../pages/superAdmin/CreateItemCategoryMenuType';
 
 const MenuManagementSAN = ({ setRenderContent }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
@@ -45,59 +45,12 @@ const MenuManagementSAN = ({ setRenderContent }) => {
       case 'CreateCategoryMenuType':
         setRenderContent(() => () => <CreateCategoryMenuType />);
         break;
-      case 'CreateItemCategoryMenuType':
-        setRenderContent(() => () => <CreateItemCategoryMenuType />);
-        break;
       default:
         setRenderContent(() => () => <p>Page not found</p>);
     }
   };
   return (
     <div className=''>
-      <ul className="flex flex-col py-4">
-        <li>
-          <button onClick={() => handleRenderContent('createMenuListTypes')} className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-white hover:text-gray-800">
-            <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-white"><i className="bx bx-home"></i></span>
-            <span className={`text-sm font-medium ${selected === 'createMenuListTypes' ? 'text-yellow-300' : ''}`}>Create Menu List Types</span>
-          </button>
-        </li>
-        <li>
-          <button onClick={() => handleRenderContent('createMenuTypes')} className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-white hover:text-gray-800">
-            <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-white"><i className="bx bx-home"></i></span>
-            <span className={`text-sm font-medium ${selected === 'createMenuTypes' ? 'text-yellow-300' : ''}`}>Create Menu Types</span>
-          </button>
-        </li>
-        <li>
-          <button onClick={() => handleRenderContent('createCategory')} className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-white hover:text-gray-800">
-            <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-white"><i className="bx bx-home"></i></span>
-            <span className={`text-sm font-medium ${selected === 'createCategory' ? 'text-yellow-300' : ''}`}>Create Categories</span>
-          </button>
-        </li>
-        <li>
-          <button onClick={() => handleRenderContent('CreateItem')} className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-white hover:text-gray-800">
-            <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-white"><i className="bx bx-home"></i></span>
-            <span className={`text-sm font-medium ${selected === 'CreateItem' ? 'text-yellow-300' : ''}`}>Create Items</span>
-          </button>
-        </li>
-        <li>
-          <button onClick={() => handleRenderContent('CreateCategoryMenuType')} className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-white hover:text-gray-800">
-            <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-white"><i className="bx bx-home"></i></span>
-            <span className={`text-sm font-medium ${selected === 'CreateCategoryMenuType' ? 'text-yellow-300' : ''}`}>Create Category Menu Type</span>
-          </button>
-        </li>
-        <li>
-          <button onClick={() => handleRenderContent('CreateItemCategoryMenuType')} className="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-white hover:text-gray-800">
-            <span className="inline-flex items-center justify-center h-12 w-12 text-lg text-white"><i className="bx bx-home"></i></span>
-            <span className={`text-sm font-medium ${selected === 'CreateItemCategoryMenuType' ? 'text-yellow-300' : ''}`}>Create Item Category Menu Type</span>
-          </button>
-        </li>
-        <li>
-          <a href="#" className="text-white hover:text-gray-800">
-            <span className="inline-flex items-center justify-center h-12 w-12 text-white"><i className="bx bx-log-out"></i></span>
-            <span className="text-sm font-medium"><Logout /></span>
-          </a>
-        </li>
-=======
       <ul class="flex flex-col py-4 ">
         <button type="button" class="fixed top-4 left-4 z-50 p-2 text-gray-500 bg-white rounded-lg shadow-md dark:bg-gray-800 dark:text-gray-400 sm:hidden" onClick={toggleSidebar}>
           <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
