a5e6e272fd40bcd9e85aea74daf6743a87d0e66a
pathuGIT <wannigelakshan@gmail.com>
2025-05-08 13:29:57 +0530

    calender view

--- frontend/src/components/bookings/BookingTable.js
+++ frontend/src/components/bookings/BookingTable.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState, useRef } from 'react';
 
 export const BookingTable = ({ bookings, onRowClick }) => {
+
+  // Format ISO dates to readable
+  const formatDate = (iso) => new Date(iso).toLocaleDateString();
+
   return (
-    <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-5">
+    <div class="relative overflow-x-auto overflow-y-scroll shadow-md sm:rounded-lg mt-5" style={{ maxHeight: '450px' }}>
       {/* display venue table */}
       <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -18,14 +22,14 @@ export const BookingTable = ({ bookings, onRowClick }) => {
           {bookings.map((booking) => (
             <tr
               key={booking.booking_id}
-              className={`cursor-pointer hover:bg-gray-300 ${booking.status === 'confirmed'  &&
+              className={`cursor-pointer hover:bg-gray-300 ${booking.status === 'confirmed' &&
                 new Date(booking.booking_date) > new Date() &&
                 'bg-yellow-100 '
                 }`}
               onClick={() => onRowClick(booking.booking_id)}
-              >
+            >
               <td className="border px-6 py-4">{booking.booking_id}</td>
-              <td className="border px-6 py-4">{booking.booking_date}</td>
+              <td className="border px-6 py-4">{formatDate(booking.booking_date)}</td>
               <td className="border px-6 py-4">{booking.status}</td>
               <td className="border px-6 py-4">{booking.customer_id}</td>
               <td className="border px-6 py-4">{booking.total_price}</td>
