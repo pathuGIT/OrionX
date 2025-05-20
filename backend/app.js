8e8cdb3ae4f55c6a97e43b731313efd7f1eb25b5
pathuGIT <wannigelakshan@gmail.com>
2025-05-20 21:02:42 +0530

    backend: system user setting

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,7 @@ import customerSelectionRoutes from "./routes/CustomerMenuSelectionRoutes.js";
 import bookingRoutes from './routes/bookingRoutes.js';
 import menuViewRoutes from './routes/menuViewRoutes.js';
 import overviewRout from './routes/overviewRoutes.js';
+import settingRoute from './routes/settingRoutes.js';
 
 const app = express();
 
@@ -39,7 +40,7 @@ app.use('/api/advanceMenuView', menuViewRoutes); //meka tmi werdi ei
 //booking routes
 app.use('/api/booking', bookingRoutes);
 app.use('/api/overview', overviewRout);
-
+app.use('/api/setting', settingRoute);
 
 //event routes
 app.use('/api/event', eventRoute);
---
+++ backend/controllers/settingController.js
@@ -0,0 +1,42 @@
+import bcrypt from 'bcrypt';
+import { assignSubAdmin, changeUserPassword, deactivateAdmin, findUserById, listAdmins, listEmployees, updateUserProfile } from '../models/settingModel.js';
+
+export const getProfile = async (req, res) => {
+  const user = await findUserById(req.params.id);
+  res.json({ name: user.name, email: user.email });
+};
+
+export const updateProfile = async (req, res) => {
+  const { name, email } = req.body;
+  await updateUserProfile(req.params.id, name, email);
+  res.sendStatus(204);
+};
+
+export const changePassword = async (req, res) => {
+  const { current, new: newPwd } = req.body;
+  // verify current omitted for brevity
+  const hash = await bcrypt.hash(newPwd, 10);
+  await changeUserPassword(req.params.id, hash);
+  res.sendStatus(204);
+};
+
+export const getAdmins = async (_req, res) => {
+  const admins = await listAdmins();
+  res.json(admins);
+};
+
+export const getEmployees = async (_req, res) => {
+  const emps = await listEmployees();
+  res.json(emps);
+};
+
+export const assignAdmin = async (req, res) => {
+  const { employeeId } = req.body;
+  const newAdmin = await assignSubAdmin(employeeId);
+  res.status(201).json(newAdmin);
+};
+
+export const deactivate = async (req, res) => {
+  await deactivateAdmin(req.params.id);
+  res.sendStatus(204);
+};
