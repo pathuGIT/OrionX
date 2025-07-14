f8468c4de5abd0aee2c18ff5650610c3cb2a9067
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2025-07-14 16:01:16 +0530

    refactor: unify addCustomerNew route to use addCustomer controller

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -26,7 +26,7 @@ import {
     notifySingleEmployeePayroll,
     sendIdToEmp,
     getPaymentHistory,
-    addCustomerNew,
+    addCustomer,
     UpdatePayStatus
 } from '../controllers/userController.js';
 
@@ -37,7 +37,7 @@ router.post('/addEmployee',superAdmin,  addEmployee);
 router.post('/addCustomer', addCustomer);
 
 //Add new customer with email - test
-router.post('/addCustomerNew', addCustomerNew);
+router.post('/addCustomerNew', addCustomer);
 
 
 router.get('/searchCustomer', searchCustomer);
